36e1dd3317048f404fb305ced1b649511d0460c4
yeprepue <yepretel@misena.edu.co>
2024-02-08 12:19:58 -0500

    Creación de proyeto y plantilla

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+    <h5>Buscar</h5>
+    <input type="text" name="search" id="search" class="form-control" placeholder="Buscar gifs....">`
+})
+export class SearchBoxComponent {
+
+}
---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent,
+    CardListComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    HomePageComponent,
+    CardListComponent
+
+  ]
+})
+export class GifsModule { }
