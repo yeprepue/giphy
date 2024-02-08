4cb2c4734183318178169ff82f0fd36a335f42bc
yeprepue <yepretel@misena.edu.co>
2024-02-08 13:15:30 -0500

    Almacenar busqueda en sidebar

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
   template: `
     <h5>Buscar</h5>
-    <input type="text" name="search" id="search" class="form-control" placeholder="Buscar gifs....">`
+    <input type="text" name="search" id="search" class="form-control"    placeholder="Buscar gifs...." (keyup.enter)="searchTag()" #txtTagInpunt>`
 })
 export class SearchBoxComponent {
+  @ViewChild('txtTagInpunt')
+  public tagInput!:ElementRef<HTMLInputElement>;
 
+  constructor(private gifsService:GifsService){}
+
+  searchTag(){
+    const newTag = this.tagInput.nativeElement.value;
+    this.gifsService.searchTag(newTag);
+
+    this,this.tagInput.nativeElement.value = '';
+
+    console.log(newTag);
+
+  }
 }
---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+  private _tagsHistory: string[] = [];
+  constructor() { }
+  get tagHistory() {
+    return [...this._tagsHistory];
+  }
+private organizeHistory(tag:string){
+  tag = tag.toLocaleLowerCase();
+  if(this._tagsHistory.includes(tag)){
+    this._tagsHistory= this._tagsHistory.filter((oldTag) => oldTag !== tag)
+  }
+  this._tagsHistory.unshift(tag);
+  this._tagsHistory = this._tagsHistory.splice(0,10);
+}
+
+  searchTag(tag: string):void {
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
+    console.log(this.tagHistory);
+  }
+}
